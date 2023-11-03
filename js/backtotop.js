e8d7e4b5b191f7adb194b65dedfbc25345846538
Jim <jimkernicky@gmail.com>
2023-11-03 16:36:07 -0400

    Refactored backtotop.js

--- js/backtotop.js
+++ js/backtotop.js
@@ -2,15 +2,20 @@ const backToTopButton = document.querySelector("#back-to-top-btn");
 
 window.addEventListener("scroll", scrollFunction);
 
+/**
+ * Display or hide back-to-top btn based on scrollY
+ */
 function scrollFunction() {
-  if (window.pageYOffset > 300) { // Show backToTopButton
+  if (window.scrollY > 300) {
+    // Show backToTopButton
     if(!backToTopButton.classList.contains("btnEntrance")) {
       backToTopButton.classList.remove("btnExit");
       backToTopButton.classList.add("btnEntrance");
       backToTopButton.style.display = "block";
     }
   }
-  else { // Hide backToTopButton
+  else {
+    // Hide backToTopButton
     if(backToTopButton.classList.contains("btnEntrance")) {
       backToTopButton.classList.remove("btnEntrance");
       backToTopButton.classList.add("btnExit");
@@ -23,30 +28,47 @@ function scrollFunction() {
 
 backToTopButton.addEventListener("click", smoothScrollBackToTop);
 
-// function backToTop() {
-//   window.scrollTo(0, 0);
-// }
-
+/**
+ * Control scroll animation
+ */
 function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
   const duration = 1250;
   let start = null;
-  
+
+  const startPosition = window.scrollY;
+  const distanceToTop = -1 * startPosition;
+
   window.requestAnimationFrame(step);
 
+  /**
+   * Synchronize animation using timestamp
+   * @param {milliseconds} timestamp 
+   */
   function step(timestamp) {
     if (!start) start = timestamp;
+
     const progress = timestamp - start;
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distanceToTop, duration));
+
     if (progress < duration) window.requestAnimationFrame(step);
   }
 }
 
-function easeInOutCubic(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
-	return c/2*(t*t*t + 2) + b;
-};
+/**
+ * 
+ * Cubic-bezier function for scroll animation
+ * 
+ * @param {milliseconds} prog 
+ * @param {pixels} startPos 
+ * @param {pixels} dist 
+ * @param {milliseconds} dur 
+ * @returns 
+ */
+function easeInOutCubic(prog, startPos, dist, dur) {
+  prog /= dur / 2;
+  if (prog < 1) {
+    return (dist / 2) * prog * prog * prog + startPos;
+  }
+  prog -= 2;
+  return (dist / 2) * (prog * prog * prog + 2) + startPos;
+}
--- js/menutoggle.js
+++ js/menutoggle.js
@@ -6,22 +6,20 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.header-menu');
 
 hamburger.addEventListener('click', () => {
-  /* Toggle active class */
+
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 
-  /* Toggle aria-expanded value */
+  // Toggle aria-expanded boolean
   let menuOpen = navMenu.classList.contains('active');
-  console.log(menuOpen);
   let newMenuOpenStatus = menuOpen;
   hamburger.setAttribute('aria-expanded', newMenuOpenStatus);
 });
 
-// close mobile menu
-document.querySelectorAll('.nav-link').forEach(n =>
-  n.addEventListener('click', () => {
+// Close mobile menu
+document.querySelectorAll('.nav-link').forEach(link =>
+  link.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-    // Need to add Toggle aria-expanded value here as well because it stays as true when you click a menu item
   })
 );
